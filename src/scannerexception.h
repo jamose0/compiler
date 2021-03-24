97cbd47f1aaec4ac0dc5363afcea350d639dce26
James Mosedale <jmdale@protonmail.com>
2021-03-23 23:19:55 -0500

    Add scannerexception files and update CMakeLists.txt

---
+++ src/scannerexception.h
@@ -0,0 +1,20 @@
+#ifndef SCANNEREXCEPTION_H
+#define SCANNEREXCEPTION_H
+
+/* ScannerException class
+*  ----------------------
+*  This class should be
+*  thrown on a lexical
+*  error
+*   ------------------- */
+
+#include <exception>
+
+class ScannerException : public std::exception
+{
+private:
+
+public:
+};
+
+#endif
