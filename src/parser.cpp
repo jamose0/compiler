5343488934fa86ee7e48b8208788760969a59dad
James Mosedale <jmdale@protonmail.com>
2021-03-28 02:03:58 -0500

    Add recursive call to block and add check to determine if at end of input (to block)

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,12 +8,17 @@ bool Parser::accept(const Token& token)
 void Parser::block()
 {
     std::cout << "calling block\n";
+    if (m_token.getType() == TokenType::END)
+        return;
+    
     if (accept(Token{TokenType::VAR, "var"}) ||
         accept(Token{TokenType::IF, "if"}) ||
         accept(Token{TokenType::WHILE, "while"}))
     {
         std::cout << "expanding to Statement!\n";
     }
+
+    block();
 }
 
 void Parser::parse()
