177b925b50f5aeb9528d2e6c599e154f014e3dc9
James Mosedale <jmdale@protonmail.com>
2021-03-30 22:44:24 -0500

    Add Parser class and accept function

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,11 @@
-#include "parser.cpp"
+#include "parser.h"
+
+bool Parser::accept(TokenType tType) const
+{
+    return tType == m_token.getType();
+}
+
+void Parser::parse()
+{
+    std::cout << "Parsing!\n";
+}
--- src/parser.h
+++ src/parser.h
@@ -1,4 +1,27 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "token.h"
+#include "scanner.h"
+
+#include <string>
+
+class Parser final
+{
+private:
+    /* m_scanner is the parser's scanner. m_token is the current token */
+    Scanner m_scanner;
+    Token m_token;
+
+    bool accept(TokenType tType) const;
+    
+public:
+    Parser(std::string_view program)
+        : m_scanner{program}, m_token{m_scanner.nextToken()}
+    {
+    }
+
+    void parse();
+};
+
 #endif
