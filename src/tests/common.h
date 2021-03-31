6972260166ca0b0ef5f3b34d23b2fe71c0006c12
James Mosedale <jmdale@protonmail.com>
2021-03-31 09:53:23 -0500

    Restructure

--- src/tests/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#include "../scanner.h"
-#include "../token.h"
-
-#include <catch2/catch.hpp>
-
-#endif
--- src/tests/testcommon.cpp
+++
@@ -1,7 +0,0 @@
-#ifndef TESTCOMMON_H
-#define TESTCOMMON_H
-
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-#endif
